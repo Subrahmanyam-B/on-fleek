922acada331c757cd32815d0d12781a5ea135d48
Zeeshan1101 <shaikzeeshan896@gmail.com>
2023-07-19 11:56:22 +0530

    Update next.config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: { esmExternals: true },
-  images: { domains: ["onfleek.agpro.co.in"] },
+  images: { domains: ["onfleek.agpro.co.in", "cms.shoponfleek.co.in"] },
 };
 
 module.exports = nextConfig;
